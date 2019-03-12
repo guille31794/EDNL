03e26f048f6774a4907a6c647c108fef4828861d
Guille31794 <guille.g94@hotmail.es>
2019-03-12 09:25:18 +0100

    Exercise 1 from practice 2, finished

--- Practicas Arboles/Arboles Binarios/Practica 2/Ejercicio1.cpp
+++ Practicas Arboles/Arboles Binarios/Practica 2/Ejercicio1.cpp
@@ -1,23 +1,29 @@
 #include "../abin.h"
 #include "../abin_E-S.h"
 
- bool simRec(Abin<T> A1, Abin<T> A2), typename Abin<T>::nodo nA1, typename Abin<T>::nodo nA2)
- {
-     if(A1.hijoIzqdoB(nA1) == NODO_NULO && A2.hijoIzqdoB(nA2) == NODO_NULO) 
+template <typename T>
+bool simRec(Abin<T> A1, Abin<T> A2, typename Abin<T>::nodo nA1, typename Abin<T>::nodo nA2)
+{
+     if (nA1 != Abin<T>::NODO_NULO && nA2 != Abin<T>::NODO_NULO) 
      {
-        
+        bool simIzq = simRec(A1, A2, A1.hijoIzqdoB(nA1), A2.hijoIzqdo(nA2));
+        bool simDer = simRec(A1, A2, A1.hijoDrcho(nA1), A2.hijoDrchoB(nA2));
+        return simIzq && simDer;          
      }
-     
- }
+     else
+         return (nA1 == Abin<T>::NODO_NULO && nA2 == Abin<T>::NODO_NULO);
+}
 
 template <typename T>
 bool similar(Abin<T> A1, Abin<T> A2)
 {
-    if(!A1.arbolVacioB() && !A2.arbolVacioB())
-       return simRec(A1, A2, A1.hijoIzqdoB(A.raizB()), A2.hijoIzqdoB(A2.raizB())) &&
-        simRec(A1, A2, A1.hijoDrchoB(A1.raizB()), A2.hijoDrchoB(A2.raizB()));
-    else if (A1.raizB() == NODO_NULO && A2.raizB() == NODO_NULO) 
+    if(A1.arbolVacioB() && A2.arbolVacioB())
         return true;
     else 
-        return false;
+        return simRec(A1, A2, A1.raizB(), A2.raizB());
+}
+
+int main()
+{
+    return 0;
 }
